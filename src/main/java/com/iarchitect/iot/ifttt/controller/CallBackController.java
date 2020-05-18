e27484135133f07b03c2bcd4ee1b99492fb5f002
Arsalan Siddiqui <arsalan_siddiqui@ch.honda.com>
2020-05-18 18:12:27 -0400

    More updates.

--- src/main/java/com/iarchitect/iot/ifttt/controller/CallBackController.java
+++ src/main/java/com/iarchitect/iot/ifttt/controller/CallBackController.java
@@ -1,56 +1,77 @@
 package com.iarchitect.iot.ifttt.controller;
 
-
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.MoreExecutors;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
-
+import java.util.concurrent.*;
 
 @RestController
 //@EnableWebMvc
 public class CallBackController extends BaseController {
-
     private final RestTemplate restTemplate;
-    String callBackUrl = "https://maker.ifttt.com/trigger/";
-    final ScheduledExecutorService executorService = Executors.newScheduledThreadPool(10);
+    //    final ScheduledExecutorService executorService = Executors.newScheduledThreadPool(10);
+    final ScheduledThreadPoolExecutor scheduledThreadPoolExecutor = new ScheduledThreadPoolExecutor(10);
+    final ScheduledExecutorService exitingScheduledExecutorService =
+            MoreExecutors.getExitingScheduledExecutorService(scheduledThreadPoolExecutor, 15, TimeUnit.MINUTES);
 
     public CallBackController(RestTemplateBuilder restTemplateBuilder) {
         this.restTemplate = restTemplateBuilder.build();
+    }
+
+    @RequestMapping(path = "/event/{eventName}/key/{apiKey}", method = RequestMethod.GET)
+    public String callNow(@PathVariable String eventName,
+                          @PathVariable String apiKey) {
+        String url = buildCallBackUrl(eventName, apiKey);
+        return this.restTemplate.getForObject(url, String.class);
     }
 
     @RequestMapping(path = "/event/{eventName}/delay/{delay}/key/{apiKey}", method = RequestMethod.GET)
     public String requestCallBack(@PathVariable String eventName,
                                   @PathVariable long delay,
                                   @PathVariable String apiKey,
-                                  @RequestParam boolean nightOnly) {
+                                  @RequestParam boolean nightOnly) throws ExecutionException, InterruptedException {
         if (nightOnly) {
             if (isNightTime()) {
-                return scheduleCallBack(eventName, delay, apiKey);
+                return scheduleCallBackAsync(eventName, delay, apiKey);
             }
         } else {
-            return scheduleCallBack(eventName, delay, apiKey);
+            return scheduleCallBackAsync(eventName, delay, apiKey);
         }
-
         return "Nothing scheduled";
     }
 
-    @NotNull
-    private String scheduleCallBack(@PathVariable String eventName, @PathVariable long delay, @PathVariable String apiKey) {
-        executorService.schedule(invokeIftttWebHook(eventName, apiKey), delay, TimeUnit.MINUTES);
-        executorService.shutdown();
+    private String scheduleCallBack(String eventName, long delay, String apiKey) throws InterruptedException {
+        Thread.sleep(delay * 60 * 1000);
+        String url = buildCallBackUrl(eventName, apiKey);
+        return this.restTemplate.getForObject(url, String.class);
+    }
+
+    private String scheduleCallBackAsync(String eventName, long delay, String apiKey) throws ExecutionException, InterruptedException {
+//        executorService.schedule(invokeIftttWebHook(eventName, apiKey), delay, TimeUnit.MINUTES);
+        // Executor service should stay up for subsequent requests
+        // executorService.shutdown();
+
+        ScheduledFuture<String> schedule = exitingScheduledExecutorService.schedule(invokeIftttWebHook(eventName, apiKey), delay, TimeUnit.MINUTES);
+//        String response = schedule.get();
+//        System.out.println(response);
         return "Call Back Requested";
     }
 
-    private Runnable invokeIftttWebHook(String eventName, String apiKey) {
-        this.callBackUrl = this.callBackUrl + eventName + "/with/key/" + apiKey;
+    private Callable<String> invokeIftttWebHook(String eventName, String apiKey) {
+        String url = buildCallBackUrl(eventName, apiKey);
         return () -> {
-            this.restTemplate.getForObject(this.callBackUrl, String.class);
+            String response = this.restTemplate.getForObject(url, String.class);
+            System.out.println("From the thread = " + response);
+            return response;
         };
+    }
 
+    @NotNull
+    private String buildCallBackUrl(@PathVariable String eventName, @PathVariable String apiKey) {
+        return "https://maker.ifttt.com/trigger/" + eventName + "/with/key/" + apiKey;
     }
 }
